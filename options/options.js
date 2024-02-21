d6cf8ab468b84a02f4756b6d0f79eb557a24e399
Crowdbotics <team@crowdbotics.com>
2024-02-21 15:26:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "guila-lion-app-46533",
+  url: "https://guila-lion-app-46533.botics.co",
+  api: "https://guila-lion-app-46533.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
